58b4a80ec6338a072f7c7cb3486ec92a1bf5bbef
☁ Rheza Satria <rheza.satria.ta@gmail.com>
2018-04-15 16:54:06 +0700

    add basic http with graceful

---
+++ pkg/gin/http_test.go
@@ -0,0 +1,26 @@
+package gin
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRun_setup(t *testing.T) {
+
+	// test that using no handler panics
+	assert.Panics(t, func() {
+		setup(Config{
+			Handler: nil,
+		})
+	})
+
+	// test defaults
+	srv := setup(Config{
+		Handler: http.NotFoundHandler(),
+	})
+
+	assert.Equal(t, DefaultShutdownGracePeriod, srv.Timeout)
+	assert.Equal(t, DefaultListenAddr, srv.Server.Addr)
+}
